0a0fd2060616dcc9d476b54f6baf5ee04e746338
Camilo Vargas <ca06vargas@gmail.com>
2026-02-11 20:17:27 -0500

    Update consts.ts to correct username and social media links

--- src/consts.ts
+++ src/consts.ts
@@ -1,25 +1,25 @@
 import type { Site, Socials } from "@/types";
 
 export const SITE: Site = {
-    NAME: "Camilo Vargas",
-    USERNAME: "vrgs_0",
-    EMAIL: "ca06vargas@gmail.com",
-    NUM_POSTS_ON_HOMEPAGE: 3,
-    NUM_WORKS_ON_HOMEPAGE: 2,
-    NUM_PROJECTS_ON_HOMEPAGE: 3,
+  NAME: "Camilo Vargas",
+  USERNAME: "cvargas_0",
+  EMAIL: "ca06vargas@gmail.com",
+  NUM_POSTS_ON_HOMEPAGE: 3,
+  NUM_WORKS_ON_HOMEPAGE: 2,
+  NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const SOCIALS: Socials = [
-    {
-        NAME: "x",
-        HREF: "https://x.com/vrgs_0",
-    },
-    {
-        NAME: "github",
-        HREF: "https://github.com/vrgs-dev"
-    },
-    {
-        NAME: "linkedin",
-        HREF: "https://www.linkedin.com/in/camilovargs/",
-    }
+  {
+    NAME: "x",
+    HREF: "https://x.com/cvargas_0",
+  },
+  {
+    NAME: "github",
+    HREF: "https://github.com/cvargas-0",
+  },
+  {
+    NAME: "linkedin",
+    HREF: "https://www.linkedin.com/in/camilovargs/",
+  },
 ];
